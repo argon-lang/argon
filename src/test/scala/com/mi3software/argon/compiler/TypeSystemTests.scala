1ca61822aac40265606c34acfbbcc49d43c16b25
argon-dev <argon@argon.dev>
2018-02-10 07:32:46 -0500

    Moved dummyTraits/Classes out of property.

--- src/test/scala/com/mi3software/argon/compiler/TypeSystemTests.scala
+++ src/test/scala/com/mi3software/argon/compiler/TypeSystemTests.scala
@@ -9,25 +9,25 @@ class TypeSystemTests extends PropSpec with PropertyChecks with Matchers {
 
   val ts = new DummyTypeSystem
 
+  private val dummyTraits = Seq(
+    ts.dummyTrait(0, Vector(0)),
+    ts.dummyTrait(1, Vector(1, 0)),
+    ts.dummyTrait(2, Vector(2, 0)),
+    ts.dummyTrait(3, Vector(3, 1, 0)),
+  )
+
+  private val dummyClasses = Seq(
+    ts.dummyClass(0, Vector(0), Vector(0)),
+    ts.dummyClass(1, Vector(1, 0), Vector(1, 0)),
+    ts.dummyClass(2, Vector(2, 0), Vector(2, 0)),
+    ts.dummyClass(3, Vector(3, 1, 0), Vector(3, 1, 0)),
+  )
+
+  private implicit def traitArb = Arbitrary[ts.DummyTrait](Gen.oneOf(dummyTraits))
+  private implicit def classArb = Arbitrary[ts.DummyClass](Gen.oneOf(dummyClasses))
+
+
   property("Types should be subtypes of themselves") {
-
-    val dummyTraits = Seq(
-      ts.dummyTrait(0, Vector(0)),
-      ts.dummyTrait(1, Vector(1, 0)),
-      ts.dummyTrait(2, Vector(2, 0)),
-      ts.dummyTrait(3, Vector(3, 1, 0)),
-    )
-
-    val dummyClasses = Seq(
-      ts.dummyClass(0, Vector(0), Vector(0)),
-      ts.dummyClass(1, Vector(1, 0), Vector(1, 0)),
-      ts.dummyClass(2, Vector(2, 0), Vector(2, 0)),
-      ts.dummyClass(3, Vector(3, 1, 0), Vector(3, 1, 0)),
-    )
-
-    implicit def traitArb = Arbitrary[ts.DummyTrait](Gen.oneOf(dummyTraits))
-    implicit def classArb = Arbitrary[ts.DummyClass](Gen.oneOf(dummyClasses))
-
     forAll { (t: ts.TType) =>
       ts.comparer.isSubType(t, t) shouldBe true
     }
