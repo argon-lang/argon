2ba0cf6f98adfecbd912d2e7ca5b1bb80bc27a6f
argon-dev <argon@argon.dev>
2019-05-27 14:50:43 -0400

    Added InputStreamStream.

---
+++ argon-util/src/main/scala/dev/argon/util/stream/InputStreamReaderTransformation.scala
@@ -0,0 +1,110 @@
+package dev.argon.util.stream
+
+import java.io.{IOException, InputStream, OutputStream, PipedInputStream, PipedOutputStream}
+import java.util.Objects
+import java.util.concurrent.{ArrayBlockingQueue, BlockingQueue}
+
+import cats.data.NonEmptyVector
+import dev.argon.util.stream
+import scalaz.zio
+import scalaz.zio.{Exit, Fiber, IO, Ref, ZIO, ZManaged}
+import scalaz.zio.blocking.Blocking
+import scalaz.zio.interop._
+
+trait InputStreamReaderTransformation[-R, +E, +X] extends StreamTransformation[ZIO, R, E, Byte, Unit, Nothing, X] {
+
+  def readDirectly[R2 <: R, E2 >: E](inputStream: InputStream): ZIO[R2, E2, X]
+
+}
+
+object InputStreamReaderTransformation {
+
+  @SuppressWarnings(Array(
+    "org.wartremover.warts.Equals",
+    "org.wartremover.warts.NonUnitStatements",
+    "org.wartremover.warts.Null",
+    "org.wartremover.warts.Var",
+  ))
+  private final class TransformInputStream(queue: BlockingQueue[Vector[Byte]]) extends InputStream {
+
+    private var remainingData: Vector[Byte] = _
+
+    override def read(): Int = {
+      val buff = new Array[Byte](1)
+      if(read(buff) < 0)
+        -1
+      else
+        buff(0).toInt
+    }
+
+    override def read(b: Array[Byte], off: Int, len: Int): Int = {
+      Objects.checkFromIndexSize(off, len, b.length)
+      if(len == 0)
+        0
+      else {
+        val data = if(remainingData eq null) queue.take() else remainingData
+        if(data eq null)
+          -1
+        else {
+          data.copyToArray(b, off, len)
+
+          remainingData = if(len >= data.size) null else data.drop(len)
+          Math.min(len, data.size)
+        }
+      }
+    }
+  }
+
+  @SuppressWarnings(Array("org.wartremover.warts.Null"))
+  def apply[R <: Blocking, E, X](errorHandler: IOException => E)(readHandler: InputStream => ZIO[R, E, X]): InputStreamReaderTransformation[R, E, X] =
+    new InputStreamReaderTransformation[R, E, X] {
+      override def readDirectly[R2 <: R, E2 >: E](inputStream: InputStream): ZIO[R2, E2, X] =
+        readHandler(inputStream)
+
+      override type State = (BlockingQueue[Vector[Byte]], Ref[Boolean], Fiber[E, X])
+
+      override def initial: Resource[ZIO, R, E, State] =
+        Resource.fromZManaged(ZManaged.make(
+          for {
+            env <- ZIO.environment[Blocking]
+            queue <- IO.effectTotal { new ArrayBlockingQueue[Vector[Byte]](32) }
+            inputStream <- IO.effectTotal { new TransformInputStream(queue) }
+
+            doneReading <- Ref.make(false)
+
+            readerTask <- readHandler(inputStream)
+              .onTermination { _ =>
+                doneReading.set(true).flatMap { _ =>
+                  ZIO.effectTotal { queue.poll() }
+                }
+              }
+              .fork
+
+          } yield (queue, doneReading, readerTask)
+
+        ) {
+          case (queue, doneReading, _) =>
+            ZIO.environment[Blocking].flatMap { env =>
+              doneReading.get.flatMap {
+                case true => IO.succeed(())
+                case false => IO.effectTotal { queue.put(null) }
+              }
+            }
+        })
+
+      override def step(s: State, ca: NonEmptyVector[Byte]): ZIO[R, E, Step[State, Byte, Nothing, X]] =
+        s._2.get.flatMap {
+          case true => s._3.join.map(Step.Stop.apply)
+          case false => IO.effectTotal {
+            s._1.put(ca.toVector)
+            Step.Continue(s)
+          }
+        }
+
+      override def end(s: State, result: Unit): ZIO[R, E, (Vector[Nothing], ZIO[R, E, X])] =
+        IO.succeed((Vector(), s._3.join))
+    }
+
+}
+
+
---
+++ argon-util/src/main/scala/dev/argon/util/stream/InputStreamStream.scala
@@ -0,0 +1,45 @@
+package dev.argon.util.stream
+
+import java.io.{IOException, InputStream}
+
+import cats.Monad
+import cats.data.NonEmptyVector
+import scalaz.zio.{IO, ZIO}
+import scalaz.zio.stream.ZStream
+
+final class InputStreamStream[E](errorHandler: IOException => E)(inputStreamResource: Resource[ZIO, Any, E, InputStream]) extends ArStream[ZIO, Any, E, Byte] {
+
+  override def foldLeft[R2 <: Any, E2 >: E, A2 >: Byte, X](trans: StreamTransformation[ZIO, R2, E2, A2, Unit, Nothing, X])(implicit monadInstance: Monad[ZIO[R2, E2, ?]]): ZIO[R2, E2, X] =
+    trans match {
+      case trans: InputStreamReaderTransformation[R2, E2, X] =>
+        inputStreamResource.use(trans.readDirectly)
+
+      case _ =>
+        inputStreamResource.use { inputStream =>
+          trans.initial.use { s =>
+            def feed(s: trans.State): ZIO[R2, E2, X] =
+              IO.effect {
+                val buffer = new Array[Byte](2049)
+                val bytesRead = inputStream.read(buffer)
+                buffer.take(bytesRead).toVector
+              }
+                .refineOrDie { case ex: IOException => errorHandler(ex) }
+                .flatMap { chunk =>
+                  NonEmptyVector.fromVector(chunk) match {
+                    case Some(chunk) => trans.step(s, chunk).flatMap {
+                      case Step.Produce(_, value, _) => value
+                      case Step.Continue(s) => feed(s)
+                      case Step.Stop(x) => IO.succeed(x)
+                    }
+                    case None => trans.end(s, ()).flatMap { case (_, fx) => fx }
+                  }
+                }
+
+            feed(s)
+          }
+        }
+    }
+
+
+  override def toZStream(toIO: ArStream.EffectConverter[ZIO, ZIO]): ZStream[Any, E, Byte] = ???
+}
