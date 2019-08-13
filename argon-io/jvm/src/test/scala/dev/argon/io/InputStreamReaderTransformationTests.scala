228d6a17cf3b47aa9f4d239896b1c31ac2b5c72b
argon-dev <argon@argon.dev>
2019-08-13 06:24:12 -0400

    Test cleanup.

--- argon-io/jvm/src/test/scala/dev/argon/io/InputStreamReaderTransformationTests.scala
+++ argon-io/jvm/src/test/scala/dev/argon/io/InputStreamReaderTransformationTests.scala
@@ -12,38 +12,47 @@ class InputStreamReaderTransformationTests extends FlatSpec with Matchers with D
 
   private val streamContent = Seq[Byte](0, 7, 5, 9, 4, -1)
 
+  private def usingSingleByteRead(inputStream: InputStream): RIO[Blocking, Seq[Byte]] =
+    ZIO.accessM[Blocking] { _.blocking.effectBlocking {
+      val allData = new ArrayBuffer[Byte]()
 
-  "InputStreamReaderTransformation" should "readDirectly using read()" in {
-    unsafeRun(InputStreamReaderTransformation { inputStream =>
-      ZIO.accessM[Blocking] { _.blocking.effectBlocking {
-        for (b <- streamContent) {
-          inputStream.read() shouldBe (b & 0xFF)
+      def readMore(): Unit = {
+        val b = inputStream.read()
+        if(b >= 0) {
+          allData += b.toByte
+          readMore()
         }
+      }
 
-        inputStream.read() shouldBe (-1)
-      } }
-    }.readDirectly[Blocking, Throwable](new ByteArrayInputStream(streamContent.toArray)))
+      readMore()
+
+      allData.toSeq
+    } }
+
+  private def usingBufferReader(inputStream: InputStream): RIO[Blocking, Seq[Byte]] =
+    ZIO.accessM[Blocking] { _.blocking.effectBlocking {
+      val allData = new ArrayBuffer[Byte]()
+
+      val buff = new Array[Byte](1024)
+      def readMore(): Unit = {
+        val bytesRead = inputStream.read(buff, 0, buff.length)
+        if(bytesRead > 0) {
+          allData ++= buff.take(bytesRead)
+          readMore()
+        }
+      }
+
+      readMore()
+
+      allData.toSeq
+    } }
+
+  "InputStreamReaderTransformation" should "readDirectly using single byte read" in {
+    unsafeRun(InputStreamReaderTransformation(usingSingleByteRead).readDirectly(new ByteArrayInputStream(streamContent.toArray))) shouldBe streamContent
   }
 
-  it should "readDirectly using read(Array[Byte], Int, Int)" in {
-    unsafeRun(InputStreamReaderTransformation { inputStream =>
-      ZIO.accessM[Blocking] { _.blocking.effectBlocking {
-        val allData = new ArrayBuffer[Byte]()
-
-        val buff = new Array[Byte](1024)
-        def readMore(): Unit = {
-          val bytesRead = inputStream.read(buff, 0, buff.length)
-          if(bytesRead > 0) {
-            allData ++= buff.take(bytesRead)
-            readMore()
-          }
-        }
-
-        readMore()
-
-        allData.toSeq shouldBe streamContent
-      } }
-    }.readDirectly[Blocking, Throwable](new ByteArrayInputStream(streamContent.toArray)))
+  it should "readDirectly using buffer reads" in {
+    unsafeRun(InputStreamReaderTransformation(usingBufferReader).readDirectly(new ByteArrayInputStream(streamContent.toArray))) shouldBe streamContent
   }
 
 
