db69ddfa12af3fcdc0b30d1e56b0f0194e6e5db6
argon-dev <argon@argon.dev>
2019-09-29 08:46:48 -0400

    Added WritableZStream test Chunk per byte.

--- argon-io/js/src/test/scala/dev/argon/io/WritableZStreamTests.scala
+++ argon-io/js/src/test/scala/dev/argon/io/WritableZStreamTests.scala
@@ -3,21 +3,28 @@ package dev.argon.io
 import zio._
 import zio.test._
 import zio.test.Assertion._
+import cats.implicits._
+import zio.interop.catz._
 
 import scalajs.js
 import scalajs.js.JSConverters._
 
 object WritableZStreamTests extends DefaultRunnableSpec({
+
+  def writeDataToWritable(writable: NodeWritable, data: List[Byte]): IO[js.Error, Unit] =
+    IO.effectAsync { register =>
+      writable.write(NodeBuffer.from(data.toArray.toJSArray.map(_.toInt)), "utf8", _ => {
+        register(IO.succeed(()))
+      })
+    }
+
+
   suite("WritableZStream")(
     testM("Single Chunk") {
       checkM(Gen.listOf(Gen.anyByte)) { byteList =>
         assertM(
           WritableZStream { writable =>
-            IO.effectAsync { register =>
-              writable.write(NodeBuffer.from(byteList.toArray.toJSArray.map(_.toInt)), "utf8", _ => {
-                register(IO.succeed(()))
-              })
-            }
+            writeDataToWritable(writable, byteList)
           }
             .runCollect
             .mapError(js.JavaScriptException)
@@ -26,5 +33,22 @@ object WritableZStreamTests extends DefaultRunnableSpec({
         )
       }
     },
+
+    testM("Chunk per byte") {
+      checkM(Gen.listOf(Gen.anyByte)) { byteList =>
+        assertM(
+          WritableZStream { writable =>
+            byteList.traverse_ { b =>
+              writeDataToWritable(writable, List(b))
+            }
+          }
+            .runCollect
+            .mapError(js.JavaScriptException)
+            .orDie,
+          equalTo(byteList.map(Chunk(_)))
+        )
+      }
+    },
+
   )
 })
