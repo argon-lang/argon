7cfe4cec42dc914b48c4d369ada92b7b1d97cb0c
argon-dev <argon@argon.dev>
2026-07-08 08:08:36 -0500

    Fix to_s extern.

--- libraries/Argon.Core/jvm/externs/argontube2/Argon/Core/Int/IntExterns.java
+++ libraries/Argon.Core/jvm/externs/argontube2/Argon/Core/Int/IntExterns.java
@@ -4,12 +4,13 @@ import java.math.BigInteger;
 
 import dev.argon.runtime.ExternFunction;
 import dev.argon.runtime.Trampoline;
+import dev.argon.runtime.Tuple0;
 
 class IntExterns {
     private IntExterns() {}
 
     @ExternFunction("int_to_s")
-    public static Trampoline<String> intToS(BigInteger i) {
+    public static Trampoline<String> intToS(BigInteger i, Tuple0 _empty) {
         return new Trampoline.Result<>(i.toString());
     }
 }
